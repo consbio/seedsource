008f6a8ad3281a0d6d77058332eef14683c749ec
nikmolnar <nik.molnar@consbio.org>
2015-12-30 07:09:21 -0800

    Remove custom.py

--- source/seedsource_project/settings/custom.py
+++
@@ -1,4 +0,0 @@
-from seedsource_project.settings.local import *
-
-NC_SERVICE_DATA_ROOT = 'C:/Users/nik.molnar/Documents/projects/seedsource/materials/ncdjango/services/'
-NC_TEMPORARY_FILE_LOCATION = 'C:/Users/nik.molnar/Documents/projects/seedsource/materials/ncdjango/tmp/'
