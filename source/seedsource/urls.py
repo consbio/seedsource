ccdbc5c28e0080bb25ad1599a9f5906eceaf856a
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-06-10 11:02:00 -0700

    Remove test page for running jobs

--- source/seedsource/urls.py
+++ source/seedsource/urls.py
@@ -10,6 +10,5 @@ router.register('run-configurations', RunConfigurationViewset)
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='tool.html'), name='tool_page'),
-    url(r'^tests/run-job/$', TemplateView.as_view(template_name='tests/run_job.html'), name='test_run_job'),
     url(r'^', include(router.urls))
 ]
