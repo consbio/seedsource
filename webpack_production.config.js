9c048a91c4fe1d4c72663c96b04959bb39bcb04d
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-06-29 14:06:34 -0700

    Add some production settings

--- webpack_production.config.js
+++ webpack_production.config.js
@@ -3,17 +3,30 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
+    devtool: 'cheap-module-source-map',
     context: __dirname,
-
     entry: './assets/index',
 
     output: {
         path: path.resolve('./source/seedsource/static/sst/build/'),
-        filename: '[name].js'
+        filename: '[name].min.js'
     },
 
     plugins: [
-        new BundleTracker({filename: './webpack-stats.json'})
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                screw_ie8: true
+            },
+            comments: false,
+            sourceMap: false
+        })
     ],
 
     module: {
