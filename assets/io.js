bbf5426d2b51b913d3d0054bc158c26f93f5fcb4
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-07-13 17:25:53 -0700

    Add new component for managing async requests, network I/O utils

---
+++ assets/io.js
@@ -0,0 +1,34 @@
+import fetch from 'isomorphic-fetch'
+import { getCookies } from './utils'
+
+export const urlEncode = data => {
+    let items = Object.keys(data).map(item => encodeURIComponent(item) + '=' + encodeURIComponent(data[item]))
+
+    return items.join('&')
+}
+
+export const get = url => {
+    return fetch(url, {credentials: 'same-origin'})
+}
+
+export const post = (url, data, options) => {
+    let { method } = options
+
+    if (!method) {
+        method = 'POST'
+    }
+
+    if (data) {
+        data = JSON.stringify(data)
+    }
+
+    return fetch(url, {
+        method,
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookies().csrftoken
+        },
+        body: data
+    })
+}
