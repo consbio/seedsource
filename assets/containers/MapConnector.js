6f28332a97b63053b65b5966e573ce1df11b959b
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-07-21 15:30:23 -0700

    Remove scheme from terrain basemap url

--- assets/containers/MapConnector.js
+++ assets/containers/MapConnector.js
@@ -57,7 +57,7 @@ class MapConnector extends React.Component {
                     maxZoom: 16,
                     subdomains: ['server', 'services']
                 }),
-                L.tileLayer('http://{s}.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+                L.tileLayer('//{s}.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
 	                attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
 	                maxZoom: 13,
                     subdomains: ['server', 'services']
