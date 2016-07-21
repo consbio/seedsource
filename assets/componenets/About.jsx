ba3faa9822cb3d0ed3c34ec696dfb64a5fbb26e8
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2016-07-21 15:13:39 -0700

    Add about tab

---
+++ assets/componenets/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        <h4>Planting Healthy Forests</h4>
+        <img src="/static/sst/images/hands.jpg" className="aboutImage" />
+        <p>
+            The seedlot selection tool (SST) is a GIS mapping program designed to help forest managers match seedlots
+            with planting sites based on climatic information. The climates of the planting sites can be chosen to
+            represent current climates, or future climates based on selected climate change scenarios.
+        </p>
+    </div>
+)
--- assets/componenets/App.jsx
+++ assets/componenets/App.jsx
@@ -4,15 +4,20 @@ import TabContent from '../containers/TabContent'
 import RunConfiguration from '../containers/RunConfiguration'
 import MapConnector from '../containers/MapConnector'
 import SavedRuns from '../containers/SavedRuns'
+import About from './About'
 
 export default () => (
     <div className="sidebar">
         <MapConnector />
         
         <ul className="nav nav-tabs">
-            <TabHeader name="configuration">Configuration</TabHeader>
+            <TabHeader name="about">About</TabHeader>
+            <TabHeader name="configuration">Tool</TabHeader>
             <TabHeader name="saves">Saved Runs</TabHeader>
         </ul>
+        <TabContent name="about">
+            <About />
+        </TabContent>
         <TabContent name="configuration">
             <RunConfiguration />
         </TabContent>
