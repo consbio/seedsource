a323ba6e999c3910f09bb20fed1f7e2c558fa3e1
Taylor Mutch <taylormutch@gmail.com>
2017-03-07 17:48:37 -0800

    Fixed populate_services command to handle per-file dimension naming.

--- source/seedsource/management/commands/populate_services.py
+++ source/seedsource/management/commands/populate_services.py
@@ -33,9 +33,9 @@ class Command(BaseCommand):
         # determine extent and lat/lon variable names from DEM
         dem_path = os.path.join(BASE_DIR, 'regions', name, '{}_dem.nc'.format(name))
         with Dataset(dem_path, 'r') as ds:
-            var_names = ds.variables.keys()
-            lat = 'lat' if 'lat' in var_names else 'latitude'
-            lon = 'lon' if 'lon' in var_names else 'longitude'
+            dims = ds.dimensions.keys()
+            lat = 'lat' if 'lat' in dims else 'latitude'
+            lon = 'lon' if 'lon' in dims else 'longitude'
             l = float(ds.variables[lon][:].min())
             b = float(ds.variables[lat][:].min())
             r = float(ds.variables[lon][:].max())
@@ -47,58 +47,76 @@ class Command(BaseCommand):
             print('Adding {}'.format(name))
             print('---')
             print('elevation')
-            dem_service = Service.objects.create(
-                name='{}_dem'.format(name),
-                data_path='regions/{name}/{name}_dem.nc'.format(name=name),
-                projection=WGS84,
-                full_extent=extent,
-                initial_extent=extent
-            )
-            with Dataset(dem_path, 'r') as ds:
-                v_min = numpy.nanmin(ds.variables['elevation'][:]).item()
-                v_max = numpy.nanmax(ds.variables['elevation'][:]).item()
-                renderer = StretchedRenderer([(v_min, Color(0, 0, 0)), (v_max, Color(255, 255, 255))])
-                variable = Variable.objects.create(
-                    service=dem_service,
-                    index=0,
-                    variable='elevation',
+            service_name = '{}_dem'.format(name)
+            if not Service.objects.filter(name__iexact=service_name).exists():
+
+                dem_service = Service.objects.create(
+                    name=service_name,
+                    data_path='regions/{name}/{name}_dem.nc'.format(name=name),
                     projection=WGS84,
-                    x_dimension=lon,
-                    y_dimension=lat,
-                    name='elevation',
-                    renderer=renderer,
-                    full_extent=extent
+                    full_extent=extent,
+                    initial_extent=extent
                 )
+                with Dataset(dem_path, 'r') as ds:
+                    v_min = numpy.nanmin(ds.variables['elevation'][:]).item()
+                    v_max = numpy.nanmax(ds.variables['elevation'][:]).item()
+                    renderer = StretchedRenderer([(v_min, Color(0, 0, 0)), (v_max, Color(255, 255, 255))])
+                    variable = Variable.objects.create(
+                        service=dem_service,
+                        index=0,
+                        variable='elevation',
+                        projection=WGS84,
+                        x_dimension=lon,
+                        y_dimension=lat,
+                        name='elevation',
+                        renderer=renderer,
+                        full_extent=extent
+                    )
+            else:
+                print('{} already exists, skipping.'.format(service_name))
 
         # Generate ClimateNA services
         with transaction.atomic():
             for year in YEARS:
+
+                # Bandaid fix for missing 1981_2010 data for ne1, remove when available
+                if name == 'ne1' and year == '1981_2010':
+                    continue
+
                 print(year)
                 print('---')
                 for var in VARS:
                     print(var)
-                    service = Service.objects.create(
-                        name='{}_{}Y_{}'.format(name, year, var),
-                        data_path='regions/{name}/{year}Y/{name}_{year}Y_{var}.nc'.format(
-                            name=name, year=year, var=var
-                        ),
-                        projection=WGS84,
-                        full_extent=extent,
-                        initial_extent=extent
-                    )
 
-                    with Dataset(os.path.join(BASE_DIR, service.data_path), 'r') as ds:
-                        v_min = numpy.nanmin(ds.variables[var][:]).item()
-                        v_max = numpy.nanmax(ds.variables[var][:]).item()
-                        renderer = StretchedRenderer([(v_min, Color(0, 0, 0)), (v_max, Color(255, 255, 255))])
-                        variable = Variable.objects.create(
-                            service=service,
-                            index=0,
-                            variable=var,
+                    service_name = '{}_{}Y_{}'.format(name, year, var)
+                    if not Service.objects.filter(name__iexact=service_name).exists():
+                        service = Service.objects.create(
+                            name=service_name,
+                            data_path='regions/{name}/{year}Y/{name}_{year}Y_{var}.nc'.format(
+                                name=name, year=year, var=var
+                            ),
                             projection=WGS84,
-                            x_dimension=lon,
-                            y_dimension=lat,
-                            name=var,
-                            renderer=renderer,
-                            full_extent=extent
+                            full_extent=extent,
+                            initial_extent=extent
                         )
+
+                        with Dataset(os.path.join(BASE_DIR, service.data_path), 'r') as ds:
+                            dims = ds.dimensions.keys()
+                            lat = 'lat' if 'lat' in dims else 'latitude'
+                            lon = 'lon' if 'lon' in dims else 'longitude'
+                            v_min = numpy.nanmin(ds.variables[var][:]).item()
+                            v_max = numpy.nanmax(ds.variables[var][:]).item()
+                            renderer = StretchedRenderer([(v_min, Color(0, 0, 0)), (v_max, Color(255, 255, 255))])
+                            variable = Variable.objects.create(
+                                service=service,
+                                index=0,
+                                variable=var,
+                                projection=WGS84,
+                                x_dimension=lon,
+                                y_dimension=lat,
+                                name=var,
+                                renderer=renderer,
+                                full_extent=extent
+                            )
+                    else:
+                        print('{} already exists, skipping.'.format(service_name))
