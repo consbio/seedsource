5447f9b18a148a0215e648fcd92b86aeb802d62f
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2017-09-13 14:58:38 -0700

    Add download GeoTIFF to UI

--- assets/actions/report.js
+++ assets/actions/report.js
@@ -1,4 +1,4 @@
-import { post } from '../io'
+import { post, get } from '../io'
 import { setError } from './error'
 import { dumpConfiguration } from './saves'
 import { reports } from '../config'
@@ -6,6 +6,11 @@ import { reports } from '../config'
 export const REQUEST_REPORT = 'REQUEST_REPORT'
 export const RECEIVE_REPORT = 'RECEIVE_REPORT'
 export const FAIL_REPORT = 'FAIL_REPORT'
+export const REQUEST_TIF_JOB = 'REQUEST_TIF_JOB'
+export const RECEIVE_TIF_JOB = 'RECEIVE_TIF_JOB'
+export const FAIL_TIF_JOB = 'FAIL_TIF_JOB'
+export const RECEIVE_TIF_JOB_STATUS = 'RECEIVE_TIF_JOB_STATUS'
+export const REQUEST_TIF_JOB_STATUS = 'REQUEST_TIF_JOB_STATUS'
 
 export const requestReport = name => {
     return {
@@ -106,6 +111,122 @@ export const createReport = name => {
                 }, null, 2)
             ))
             dispatch(failReport())
+        })
+    }
+}
+
+export const requestTIFJob = () => {
+    return {
+        type: REQUEST_TIF_JOB
+    }
+}
+
+export const receiveTIFJob = ({ uuid }) => {
+    return {
+        type: RECEIVE_TIF_JOB,
+        uuid
+    }
+}
+
+export const failTIFJob = () => {
+    return {
+        type: FAIL_TIF_JOB
+    }
+}
+
+export const createTIFJob = () => {
+    return (dispatch, getState) => {
+        dispatch(requestReport('tif'))
+        dispatch(requestTIFJob())
+
+        let inputs = {
+            service_id: getState().job.serviceId
+        }
+        let data = {
+            job: 'write_tif',
+            inputs: JSON.stringify(inputs)
+        }
+
+        return post('/geoprocessing/rest/jobs/', data).then(response => {
+            let { status } = response
+
+            if (status >= 200 && status < 300) {
+                return response.json()
+            }
+            else {
+                throw new Error('Bad status creating job: ' + response.status)
+            }
+        }).then(json => dispatch(receiveTIFJob(json))).catch(err => {
+            console.log(err)
+
+            dispatch(failTIFJob())
+            dispatch(failReport())
+            dispatch(setError(
+                'Export error', 'Sorry, there was an error creating the GeoTIFF dataset.', JSON.stringify({
+                    action: 'createTIFJob',
+                    error: err ? err.message : null,
+                    inputs
+                }, null, 2)
+            ))
+        })
+    }
+}
+
+export const receiveTIFJobStatus = json => {
+    return {
+        type: RECEIVE_TIF_JOB_STATUS,
+        status: json.status
+    }
+}
+
+export const requestTIFJobStatus = () => {
+    return {
+        type: REQUEST_TIF_JOB_STATUS
+    }
+}
+
+export const fetchTIFJobStatus = jobId => {
+    return dispatch => {
+        let url = '/geoprocessing/rest/jobs/' + jobId + '/'
+
+        dispatch(requestTIFJobStatus())
+
+        return get(url).then(response => {
+            let { status } = response
+
+            if (status >= 200 && status < 300) {
+                return response.json()
+            }
+            else {
+                throw new Error('Bad status polling job: ' + response.status)
+            }
+        }).then(json => {
+            if (json.status === 'failure') {
+                dispatch(failTIFJob())
+                dispatch(failReport())
+                dispatch(setError(
+                    'Export error', 'Sorry, there was an error creating the GeoTIFF dataset.', JSON.stringify({
+                        action: 'fetchTIFJobStatus',
+                        response: json,
+                        jobId
+                    }, null, 2)
+                ))
+
+                return
+            }
+            else if (json.status === 'success') {
+                dispatch(receiveReport())
+                let { filename } = JSON.parse(json.outputs)
+                window.location = '/downloads/' + filename
+            }
+
+            dispatch(receiveTIFJobStatus(json))
+        }).catch(err => {
+            console.log(err)
+
+            dispatch(failTIFJob())
+            dispatch(failReport())
+            dispatch(setError('Export error', 'Sorry, there was a problem getting the job status.'))
         })
     }
 }
