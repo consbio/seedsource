3dcd9f4b9126c6f8248b4c939123180f90f918cd
Nik Molnar <nik.molnar@consbio.org>
2017-10-14 13:09:59 -0700

    Add new elevation bands

--- source/seedsource/management/commands/calculate_zone_transfers.py
+++ source/seedsource/management/commands/calculate_zone_transfers.py
@@ -48,6 +48,12 @@ class Command(BaseCommand):
 
             return ((x + start, x + increment + start) for x in range(low, high, increment))
 
+        def every_500(zone_id, low, high):
+            return _every(low, high, 500)
+
+        def every_1000(zone_id, low, high):
+            return _every(low, high, 1000)
+
         def historical(zone_id, low, high):
             return _every(low, high, 500)
 
